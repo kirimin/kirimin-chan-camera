b120b53512fa8e3a1adb44a7023020329b3015a1
kirimin <minkarakirimade@gmail.com>
2018-12-12 00:21:37 +0900

    hotfix for wrong density on 9.0

--- app/src/main/java/me/kirimin/kirimin_chan_camera/EditActivity.kt
+++ app/src/main/java/me/kirimin/kirimin_chan_camera/EditActivity.kt
@@ -147,6 +147,7 @@ class EditActivity : AppCompatActivity() {
             val frameBitmap = (frameImageVIew.drawable as BitmapDrawable).bitmap
             val frameSize = computeBitmapSizeFromDynamicImageLayer(frameImageVIew)
             val scaledFrameBitmap = Bitmap.createScaledBitmap(frameBitmap, frameSize.x, frameSize.y, true)
+            scaledFrameBitmap.density = 420
             canvas.drawBitmap(scaledFrameBitmap, frameImageVIew.left.toFloat(), frameImageVIew.top.toFloat(), null)
         }
 
